7e47c67aafe8c95e81ec4a59cafe8e11f04a5a24
Mohsin Ansari <mohsinansari0705@gmail.com>
2026-03-07 18:52:36 +0530

    added universal colors and sizes in utils

---
+++ client/common/utils/Sizes.ts
@@ -0,0 +1,15 @@
+export const space = {
+    sm: 8,
+    md: 16,
+    lg: 24,
+    xl: 32,
+    xxl: 40
+};
+
+export const font = {
+    sm: 8,
+    md: 16,
+    lg: 24,
+    xl: 32,
+    xxl: 40
+};
