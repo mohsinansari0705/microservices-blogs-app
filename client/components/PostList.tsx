911ba5526f9bfb82303ff0968105f257e65f3224
Mohsin Ansari <mohsinansari0705@gmail.com>
2026-03-08 02:39:19 +0530

    [client/posts] post List added to show the posts

---
+++ client/components/PostList.tsx
@@ -0,0 +1,86 @@
+import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import {
+    CardView,
+    VBox,
+    HBox
+} from 'react-native-boxes';
+import { Text, ScrollView } from 'react-native';
+import { colors } from '../common/utils/Colors';
+import { font, space } from '../common/utils/Sizes';
+
+
+export default function PostList() {
+    const [posts, setPosts] = useState({});
+
+    const fetchPosts = async () => {
+        const res = await axios.get('http://localhost:4000/posts');
+
+        setPosts(res.data);
+    };
+
+    useEffect(() => {
+        fetchPosts();
+    }, []);
+
+    const renderedPosts = Object.values(posts).map((post: any) => ({
+        id: post.id,
+        title: post.title,
+        content: post.content
+    }));
+
+
+    const renderPosts = () => (
+        <ScrollView
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{
+                flexDirection: 'column',
+                gap: space.md,
+                paddingBottom: space.lg
+            }}
+        >
+            {renderedPosts.map((post: { id: string, title: string, content: string }) =>
+                <VBox
+                    key={post.id}
+                    style={{
+                        width: '100%',
+                        backgroundColor: colors.lightBlue,
+                        borderRadius: space.sm,
+                        paddingHorizontal: space.md,
+                        paddingVertical: space.sm * 1.25
+                    }}
+                >
+                    <Text style={{
+                        color: colors.white,
+                        fontSize: font.md,
+                        fontWeight: '400',
+                        fontFamily: 'verdana',
+                        paddingBottom: space.sm
+                    }}>
+                        {post.title}
+                    </Text>
+                    <Text style={{
+                        color: colors.textMuted,
+                        fontSize: font.md
+                    }}>
+                        {post.content}
+                    </Text>
+                </VBox>
+            )}
+        </ScrollView>
+    );
+
+    return (
+        <CardView style={{
+            backgroundColor: colors.darkBlue,
+            padding: 0,
+            margin: 0
+        }}>
+            <HBox style={{ justifyContent: 'flex-start', alignItems: 'center', paddingBottom: space.sm }}>
+                <Text style={{ color: 'white', fontSize: font.lg, fontWeight: 'bold' }}>Posts</Text>
+            </HBox>
+
+            {renderPosts()}
+        </CardView>
+    );
+};
