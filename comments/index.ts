ee02afbdf5e4feab171f1bf80c4241bd65569bdf
Mohsin Ansari <mohsinansari0705@gmail.com>
2026-03-10 21:11:03 +0530

    [comments] post request added to event-bus

--- comments/index.ts
+++ comments/index.ts
@@ -1,4 +1,5 @@
 import cors from 'cors';
+import axios from 'axios';
 import express from 'express';
 import bodyParser from 'body-parser';
 import { randomBytes } from 'node:crypto';
@@ -10,28 +11,38 @@ app.use(cors());
 
 type Comment = {
     id: string;
-    comment: string;
+    content: string;
 };
 const commentsByPostId: Record<string, Comment[]> = {};
+
 
 app.get('/posts/:id/comments', (req, res) => {
     res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
     const commentId = randomBytes(4).toString('hex');
-    const { comment } = req.body;
+    const { content } = req.body;
 
     const comments = commentsByPostId[req.params.id] || [];
 
-    comments.push({ id: commentId, comment: comment });
+    comments.push({ id: commentId, content: content });
 
     commentsByPostId[req.params.id] = comments;
+
+    await axios.post('http://localhost:4005/events', {
+        type: 'CommentCreated',
+        data: {
+            id: commentId,
+            content: content,
+            postId: req.params.id
+        }
+    });
 
     res.status(201).send(comments);
 });
 
 
 app.listen(4001, () => {
-    console.log("Listening on 4001...");
+    console.log("Comments service listening on port 4001...");
 });
