440a0164559d24524d4d846e5e5a478ca7860033
Mohsin Ansari <mohsinansari0705@gmail.com>
2026-03-11 12:03:41 +0530

    [comments/events] Event-Bus post request handled

--- comments/index.ts
+++ comments/index.ts
@@ -42,6 +42,11 @@ app.post('/posts/:id/comments', async (req, res) => {
     res.status(201).send(comments);
 });
 
+app.post('/events', (req, res) => {
+    console.log('Event Received:', req.body.type);
+
+    res.send({});
+});
 
 app.listen(4001, () => {
     console.log("Comments service listening on port 4001...");
